2adad7a58ecf40dee5d3a6ceec1770cf654e7dd0
Prithivi <prithivipappian@gmail.com>
2025-04-30 06:09:46 +0000

    upto add record updated

--- src/Pages/DashBoard/DashContact.jsx
+++ src/Pages/DashBoard/DashContact.jsx
@@ -1,22 +1,16 @@
 function DashContact(props) {
-// console.log(props.data.name)
-// const name = props.data.name
+
   return (
     <>
-    
-    
-        
         <div className=" d-sm-flex align-items-center p-3 mb-3 bg-light">
             <div className="">
-                {/* <p className="fw-medium">{name}</p> */}
-                {/* <p className="text-secondary m-0 fw-light">{props.data.relation}</p> */}
+                <p className="fw-medium">{props.data&&props.data.name}</p>
+                <p className="text-secondary m-0 fw-light">{props.data&&props.data.relation}</p>
             </div>
             <div className="ms-auto">
-                {/* <p className="text-primary">{props.data.phone}</p> */}
+                <p className="text-primary">{props.data&&props.data.phone}</p>
             </div>
         </div>
-        
-    
     </>
   )
 }
